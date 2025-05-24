7cd9ddbef6a780b07b7b2eda98996750acd64f21
Gerardo <gerardo.conexiones.ugarte@gmail.com>
2025-05-24 00:12:41 -0500

    Mockups_Gerardo

--- frontend/lib/pages/add_service/add_service_controller.dart
+++ frontend/lib/pages/add_service/add_service_controller.dart
@@ -1,3 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AddServiceController extends GetxController {}
+class AddServiceController extends GetxController {
+    RxString view = ''.obs;
+
+  void gotoRegisterService (BuildContext context , bool volver){
+    Navigator.pushNamed(context, '/registerService', arguments: volver);
+  }
+  
+  
+  
+
+
+}

e657e028bbbfa5970722e97f92e6564a6e1f9bd9
Torvic13 <torvic123.vcc45@gmail.com>
2025-06-22 23:41:48 -0500

    controller y models de address y company

--- backend/app/controllers/provider_controller.js
+++ backend/app/controllers/provider_controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+//const User = require("../models/user");
 const sequelize = require("../../config/database");
 const { jwtMiddleware } = require("../../config/middlewares");
 const router = express.Router();

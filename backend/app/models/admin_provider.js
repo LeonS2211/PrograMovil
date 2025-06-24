8418d55fbed327685ccb51eed2fe23692a357c2c
LeoPC2004 <lmpc995205@gmail.com>
2025-06-23 20:09:11 -0500

    avance

--- backend/app/models/admin_provider.js
+++ backend/app/models/admin_provider.js
@@ -1,39 +1,46 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/database');
-const Question = require('./question'); // Importamos el modelo Question
+const Admin = require('./admin'); // Importamos el modelo Admin
+const Provider = require('./provider'); // Importamos el modelo Provider
 
-const Alternative = sequelize.define('Alternative', {
+const AdminProvider = sequelize.define('AdminProvider', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false
     },
-    statement: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    correct: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-    },
-    question_id: {
+    admin_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
-            model: Question,
-            key: 'id'
+            model: Admin,  // Se hace referencia al modelo Admin
+            key: 'id'      // El campo al que se hace referencia es 'id' en Admin
+        }
+    },
+    provider_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Provider,  // Se hace referencia al modelo Provider
+            key: 'id'         // El campo al que se hace referencia es 'id' en Provider
         }
     }
 }, {
-    tableName: 'alternatives',
+    tableName: 'admin_providers',
     timestamps: false
 });
 
-// Definimos la relación con Question
-Alternative.belongsTo(Question, {
-    foreignKey: 'question_id',
-    as: 'question' // Alias opcional para usar en consultas
+// Definimos la relación con Admin
+AdminProvider.belongsTo(Admin, {
+    foreignKey: 'admin_id',
+    as: 'admin' // Alias opcional para usar en consultas
 });
 
-module.exports = Alternative;
+// Definimos la relación con Provider
+AdminProvider.belongsTo(Provider, {
+    foreignKey: 'provider_id',
+    as: 'provider' // Alias opcional para usar en consultas
+});
+
+module.exports = AdminProvider;
